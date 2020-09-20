22b1554aed7ad7eff284a5861e582cf8b7269797
sushantasinha <25884819+sushantasinha@users.noreply.github.com>
2020-09-20 17:03:29 +0100

    Add PoC

---
+++ KafkaPractice/src/main/java/ProducerDemo.java
@@ -0,0 +1,40 @@
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    //https://kafka.apache.org/documentation/#producerconfigs
+    public static void main(String[] args) {
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        // Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        //Key and value serialize: Help producer to know what type of values we are sending to Kafka
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // Create the producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "Hello World");
+
+        // Send data - asynchronous
+        producer.send(record);
+
+        // flush data - Need to flush to send data to Kafka
+        producer.flush();
+
+        // flush and close producer
+        producer.close();
+
+
+    }
+}
